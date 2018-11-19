c3520e3dd848a18a93a5cc619521670553bdcfb0
Diana Romanovsky <letsshitrainbows@gmail.com>
2018-11-19 03:26:07 +0000

    built en key line added for first widget; added button to bottom of widget using fancy jQuery 'before' feature; on-going experimentation with other features and possibilities

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+
+  let answer = document.querySelector("#answer-template");
+  let container = document.querySelector("#answer-container");
+  let button = document.querySelector("#button-template");
+
+  container.appendChild(button.content.cloneNode(true));
+
+  add_line();
+
+  $(document).on('keypress', function(e){
+    keycode = (e.keyCode ? e.keyCode : e.which);
+    if (keycode === 13) {
+      add_line();
+    };
+  });
+
+  function add_line() {
+    // container.appendChild(answer.content.cloneNode(true));
+    $('#superbutton').before(answer.content.cloneNode(true));
+    line = $(container).last();
+    $('.kittens')[0].focus();
+  }
+
+// get focus working
+// button always on the bottom row
+// talk about how mobile works
+// convert from templates to interpolated string functions
+//
+
+});
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :goals
   resources :entries
   devise_for :users
+  get 'cards', to: 'home#cards'
   root to: 'home#index'
 end
+
