991061397a424fabcb7a015932c2cd1010859075
Diana Romanovsky <letsshitrainbows@gmail.com>
2018-11-20 03:27:49 +0000

    added javascript logic to home.js to collect answers from journal prompts, format them, and return a JSON object

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -15,19 +15,52 @@ $(document).ready(function() {
     };
   });
 
+  //adds extra input line after a user hits 'enter' and focuses on the new line
   function add_line() {
     // container.appendChild(answer.content.cloneNode(true));
     $('#superbutton').before(answer.content.cloneNode(true));
     // line = $(container).last();
-    let lines = $('.grateful-input');
+    let lines = $('.answer-input');
     lines[lines.length-1].focus();
   }
 
-// get focus working
-// button always on the bottom row
+  //collects data from question and returns JSON
+  function collect_answers() {
+
+    let collected_entry = {
+      answers: []
+    };
+
+    let lines = $('.answer-input');
+    for (let line of lines) {
+      let answer_object = {};
+      answer_object.question = 5;
+      answer_object.body = line.value;
+      collected_entry.answers.push(answer_object);
+    }
+    entry_json = JSON.stringify(collected_entry);
+    console.log(entry_json);
+  }
+
+  $('.submit-button').on('click', function() {
+    collect_answers();
+  })
+
+  function show_next() {
+
+  }
+  //collect_answers();
+
 // talk about how mobile works
 // convert from templates to interpolated string functions
-//
+
+
+// function onSubmit( form ){
+//   var data = JSON.stringify( $(form).serializeArray() ); //  <-----------
+
+//   console.log( data );
+//   return false; //don't submit
+// }
 
 });
 
