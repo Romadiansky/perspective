bf14c80bd20dd52510e34bddbbb106a7b99b6f06
Diana Romanovsky <letsshitrainbows@gmail.com>
2018-11-20 22:45:08 +0000

    refactored the entry UI code to place question specific logic in a prompt class

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,5 +1,51 @@
 $(document).ready(function() {
-//prompts_arr is filled with placeholder data; to be filled with db data
+
+
+  let answer = document.querySelector("#answer-template");
+  let container = document.querySelector("#answer-container");
+  let button = document.querySelector("#button-template");
+
+  class Prompt_handler_text_lines {
+    constructor(question_id) {
+
+      this.question_id = question_id
+      this.add_line();
+
+      $(document).on('keypress', (e) => {
+        // TODO: THIS WILL PROBABLY BREAK BECAUSE THE
+        // EVENT HANDLER WILL PERSIST AFTER THIS PROMPT
+        // HANDLER HAS BEEN REPLACED.  Need to find a way
+        // to remove this handler
+        let keycode = (e.keyCode ? e.keyCode : e.which);
+        if (keycode === 13) {
+          this.add_line();
+        }
+      });
+    }
+
+    //adds extra input line after a user hits 'enter' and focuses on the new line
+    add_line() {
+      $('#superbutton').before(answer.content.cloneNode(true));
+      let lines = $('.answer-input');
+      lines[lines.length-1].focus();
+    }
+
+    collect_answers() {
+
+      let answers = [];
+
+      let lines = $('.answer-input');
+      for (let line of lines) {
+        let answer_object = {};
+        answer_object.question = this.question_id;
+        answer_object.body = line.value;
+        answers.push(answer_object);
+      }
+      return answers
+    }
+  };
+
+  //prompts_arr is filled with placeholder data; to be filled with db data
   let prompts_list = [
     {
       title: "How are you feeling right now?",
@@ -13,47 +59,30 @@ $(document).ready(function() {
       interface_name: "text_list"
     }
   ];
-  let answer = document.querySelector("#answer-template");
-  let container = document.querySelector("#answer-container");
-  let button = document.querySelector("#button-template");
 
   container.appendChild(button.content.cloneNode(true));
 
-  add_line();
+  // TEMPORARY - REMOVE ME
+  new Prompt_handler_text_lines(5)
 
-  $(document).on('keypress', function(e){
-    keycode = (e.keyCode ? e.keyCode : e.which);
-    if (keycode === 13) {
-      add_line();
-    };
-  });
-
-  //adds extra input line after a user hits 'enter' and focuses on the new line
-  function add_line() {
-    // container.appendChild(answer.content.cloneNode(true));
-    $('#superbutton').before(answer.content.cloneNode(true));
-    // line = $(container).last();
-    let lines = $('.answer-input');
-    lines[lines.length-1].focus();
-  }
 
   //collects data from question and returns JSON
-  function collect_answers() {
+  // function collect_answers() {
 
-    let collected_entry = {
-      answers: []
-    };
+  //   let collected_entry = {
+  //     answers: []
+  //   };
 
-    let lines = $('.answer-input');
-    for (let line of lines) {
-      let answer_object = {};
-      answer_object.question = 5;
-      answer_object.body = line.value;
-      collected_entry.answers.push(answer_object);
-    }
-    entry_json = JSON.stringify(collected_entry);
-    console.log(entry_json);
-  }
+  //   let lines = $('.answer-input');
+  //   for (let line of lines) {
+  //     let answer_object = {};
+  //     answer_object.question = 5;
+  //     answer_object.body = line.value;
+  //     collected_entry.answers.push(answer_object);
+  //   }
+  //   entry_json = JSON.stringify(collected_entry);
+  //   console.log(entry_json);
+  // }
 
   $('.submit-button').on('click', function() {
     collect_answers();
