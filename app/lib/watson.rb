b2f1a802d3fe72268d72acfb252d1b747a6a155b
Jeff E <jeffreyestany.gb@gmail.com>
2018-11-24 00:13:43 +0000

    analysis object builder takes input

--- app/lib/watson.rb
+++ app/lib/watson.rb
@@ -32,38 +32,20 @@ class Watson
     puts "=WATSON=============================================="
     puts ids
     puts jason
+    watson_enter_entries(ids, jason)
   end
 
-  def enter_entries(a, b, c, d, e, f, answer_object)
+  def watson_enter_entries(ids, answer_object)
     # takes 6 prompt id strings (a..f) and a JSON object and inserts
     # all answers from the object (described in process_entries) to the database.
+    payload = []
     answer_object["answers"].each do |answer|
-      puts answer.inspect
-      case answer["question"]
-        when 1
-          prompt = Prompt.find(a.to_i)
-          answerbuilder = prompt.answers.create!(prompt_id: a.to_i)
-        when 2
-          prompt = Prompt.find(b.to_i)
-          answerbuilder = prompt.answers.create!(prompt_id: b.to_i)
-        when 3
-          prompt = Prompt.find(c.to_i)
-          answerbuilder = prompt.answers.create!(prompt_id: c.to_i)
-        when 4
-          prompt = Prompt.find(d.to_i)
-          answerbuilder = prompt.answers.create!(prompt_id: d.to_i)
-        when 5
-          prompt = Prompt.find(e.to_i)
-          answerbuilder = prompt.answers.create!(prompt_id: e.to_i)
-        when 6
-          prompt = Prompt.find(f.to_i)
-          answerbuilder = prompt.answers.create!(prompt_id: f.to_i)
-        else
-          puts "================= ERROR ================="
+      if !answer.second["body"]
+      else
+        payload << answer.second
       end
-      answerbuilder.body = answer["body"]
-      answerbuilder.user_id = @user.id
-      answerbuilder.save
     end
+    analysis = @user.analyses.create(entry_id: ids[:id], user_input: payload)
+    analysis.save
   end
 end
