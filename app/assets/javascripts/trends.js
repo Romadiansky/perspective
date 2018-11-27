6d5e0d24a6fc2422581304fc19bd05bef1efa4bb
Jeff E <jeffreyestany.gb@gmail.com>
2018-11-27 07:53:28 +0000

    added card progression to trends page, added basic word cloud functionality

--- app/assets/javascripts/trends.js
+++ app/assets/javascripts/trends.js
@@ -1,5 +1,129 @@
 $(document).ready(function() {
 
+  let dissonance_template = document.querySelector("#dissonance-template");
+
+  class Card_handler_dissonance_chart {
+    constructor() {
+      let new_content = dissonance_template.content.cloneNode(true);
+      content_container.appendChild(new_content);
+    }
+
+    cleanup() {
+      $(content_container).empty();
+    }
+  };
+
+
+  let wordcloud2_template = document.querySelector("#wordcloud2-template");
+  class Card_handler_wordcount2 {
+    constructor() {
+      let new_content = wordcloud2_template.content.cloneNode(true);
+      content_container.appendChild(new_content);
+    }
+
+    cleanup() {
+      $(content_container).empty();
+    }
+  };
+
+
+  let wordcloud3_template = document.querySelector("#wordcloud3-template");
+  class Card_handler_wordcount3 {
+    constructor() {
+      let new_content = wordcloud3_template.content.cloneNode(true);
+      content_container.appendChild(new_content);
+    }
+
+    cleanup() {
+      $(content_container).empty();
+    }
+  };
+
+
+  let wordcloud4_template = document.querySelector("#wordcloud4-template");
+  class Card_handler_wordcount4 {
+    constructor() {
+      let new_content = wordcloud4_template.content.cloneNode(true);
+      content_container.appendChild(new_content);
+    }
+
+    cleanup() {
+      $(content_container).empty();
+    }
+  };
+
+
+  let wordcloud5_template = document.querySelector("#wordcloud5-template");
+  class Card_handler_wordcount5 {
+    constructor() {
+      let new_content = wordcloud5_template.content.cloneNode(true);
+      content_container.appendChild(new_content);
+    }
+
+    cleanup() {
+      $(content_container).empty();
+    }
+  };
+
+  let card_list = [
+    {
+      title: "DISSONANCE",
+      subtitle: "FILL ME IN",
+      handler_class: Card_handler_dissonance_chart
+    },
+    {
+      title: "WORDCLOUD2",
+      subtitle: "FILL ME IN",
+      handler_class: Card_handler_wordcount2
+    },
+    {
+      title: "WORDCLOUD3",
+      subtitle: "FILL ME IN",
+      handler_class: Card_handler_wordcount3
+    },    {
+      title: "WORDCLOUD4",
+      subtitle: "FILL ME IN",
+      handler_class: Card_handler_wordcount4
+    },    {
+      title: "WORDCLOUD5",
+      subtitle: "FILL ME IN",
+      handler_class: Card_handler_wordcount5
+    },
+  ];
+
+  let current_card_handler;
+  let current_card_index = 0;
+  let content_container = document.querySelector("#perspective-content-container");
+
+  function load_card(index) {
+    let card_info = card_list[index];
+    $('.title').html(card_info.title);
+    $('.advice').html(card_info.subtitle);
+    $('.advice').addClass('anim-typewriter')
+    current_card_handler = new card_info.handler_class();
+  }
+
+  function next_card() {
+    current_card_handler.cleanup();
+    current_card_index ++;
+
+    if (current_card_index < card_list.length) {
+      $('.advice').removeClass('anim-typewriter')
+      $('.container').animateCss('slideOutUpBig', function(e) {
+        load_card(current_card_index);
+        $('.container').animateCss('slideInUpBig');
+      });
+    }
+  }
+
+  load_card(current_card_index);
+
+  $('.submit-button').click(next_card);
+
+
+
+  // DISSONANCE CHARTING
+
   function setDissonanceBar(percentage) {
     $('.diss-bar-dissonant').css('height', `${100-percentage}%`);
   }
@@ -10,12 +134,12 @@ $(document).ready(function() {
   google.charts.setOnLoadCallback(drawChart);
 
   function drawChart() {
-
     var data = google.visualization.arrayToDataTable([
       ['Entries', 'Detected Dissonance'],
       ['total entries',     total_entries],
       ['dissonance detected', dissonance_total]
     ]);
+    console.log('TODO: uncomment the "total entries" and "dissonance_total" lines above, and debug')
 
     var options = {
       title: 'Dissonance Pie'
@@ -26,7 +150,33 @@ $(document).ready(function() {
     chart.draw(data, options);
   }
 
+
+
 })
+
+$.fn.extend({
+  animateCss: function(animationName, callback) {
+    let animationEnd = (function(el) {
+      let animations = {
+        animation: 'animationend',
+        OAnimation: 'oAnimationEnd',
+        MozAnimation: 'mozAnimationEnd',
+        WebkitAnimation: 'webkitAnimationEnd',
+      };
+      for (let t in animations) {
+        if (el.style[t] !== undefined) {
+          return animations[t];
+        }
+      }
+    })(document.createElement('div'));
+    this.addClass('animated ' + animationName).one(animationEnd, function() {
+      $(this).removeClass('animated ' + animationName);
+      if (typeof callback === 'function') callback();
+    });
+    return this;
+  },
+});
+
 
 // TODO
 // - past entries!!!
