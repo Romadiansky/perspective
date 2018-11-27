a26a364a8a3fce0677b1e6f9253d52919041b65a
Diana Romanovsky <letsshitrainbows@gmail.com>
2018-11-27 04:11:12 +0000

    registration works; validation works; error messaging works; you can create an entry AND THEN SIGN UP!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   # NOTE: THIS MUST BE RE-ENABLED FOR PRODUCTION
   # protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    attributes = [:email, :first_name ,:last_name]
+    devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+    devise_parameter_sanitizer.permit(:account_update, keys: attributes)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     resource :answers, only: [:show]
   end
 
-  devise_for :users
+  devise_for :users, :controllers => {sessions: 'sessions', registrations: 'registrations', passwords: 'passwords'}
 
   get 'cards', to: 'home#cards'
   root to: 'home#index'
