ba7db5456e382bee2717a07e2e0a1fb6d719da2c
Jeff E <jeffreyestany.gb@gmail.com>
2018-11-27 06:18:22 +0000

    updated Operative class to filter nil entries

--- app/lib/operative.rb
+++ app/lib/operative.rb
@@ -1,19 +1,24 @@
 class Operative
 
   def self.fetch_operatives(text)
-    natural_language_understanding = IBMWatson::NaturalLanguageUnderstandingV1.new(
-      iam_apikey: ENV['WATSON_UNDERSTANDING'],
-      version: "2018-03-16"
-    )
+    if text.strip === ""
+      puts "NO TEXT"
+      return nil
+    else
+      natural_language_understanding = IBMWatson::NaturalLanguageUnderstandingV1.new(
+        iam_apikey: ENV['WATSON_UNDERSTANDING'],
+        version: "2018-03-16"
+      )
 
-    understanding = natural_language_understanding.analyze(
-      language: "en",
-      text: text,
-      features: {
-        "entities" => {},
-        "keywords" => {}
-      }
-    ).result
+      understanding = natural_language_understanding.analyze(
+        language: "en",
+        text: text,
+        features: {
+          "entities" => {},
+          "keywords" => {}
+        }
+      ).result
+    end
   end
 
   # def self.operative_words(json)
@@ -45,44 +50,48 @@ class Operative
 
   def self.enter_nlu(nlu_output, q_id, user_id)
     @user = User.find(user_id)
-    # Question 2
-    if q_id == 2
-      @q2wc = @user.word_counts.find_or_create_by(question_id: 2)
-      if nlu_output["keywords"].any?
-        nlu_output["keywords"].each do |word|
-          @q2wc.word_counter ||= {}
-          @q2wc.word_counter[word["text"]] ||= 0
-          @q2wc.word_counter[word["text"]] += word["count"]
-        end
-        if !nlu_output["entites"]
-        else
-          nlu_output["entites"].each do |word|
+    if nlu_output == nil
+      return nil
+    else
+      # Question 2
+      if q_id == 2
+        @q2wc = @user.word_counts.find_or_create_by(question_id: 2)
+        if nlu_output["keywords"].any?
+          nlu_output["keywords"].each do |word|
             @q2wc.word_counter ||= {}
             @q2wc.word_counter[word["text"]] ||= 0
             @q2wc.word_counter[word["text"]] += word["count"]
           end
+          if !nlu_output["entites"]
+          else
+            nlu_output["entites"].each do |word|
+              @q2wc.word_counter ||= {}
+              @q2wc.word_counter[word["text"]] ||= 0
+              @q2wc.word_counter[word["text"]] += word["count"]
+            end
+          end
+          @q2wc.save
         end
-        @q2wc.save
       end
-    end
-    # Question 5
-    if q_id == 5
-      @q5wc = @user.word_counts.find_or_create_by(question_id: 5)
-      if nlu_output["keywords"].any?
-        nlu_output["keywords"].each do |word|
-          @q5wc.word_counter ||= {}
-          @q5wc.word_counter[word["text"]] ||= 0
-          @q5wc.word_counter[word["text"]] += word["count"]
-        end
-        if !nlu_output["entites"]
-        else
-          nlu_output["entites"].each do |word|
+      # Question 5
+      if q_id == 5
+        @q5wc = @user.word_counts.find_or_create_by(question_id: 5)
+        if nlu_output["keywords"].any?
+          nlu_output["keywords"].each do |word|
             @q5wc.word_counter ||= {}
             @q5wc.word_counter[word["text"]] ||= 0
             @q5wc.word_counter[word["text"]] += word["count"]
           end
+          if !nlu_output["entites"]
+          else
+            nlu_output["entites"].each do |word|
+              @q5wc.word_counter ||= {}
+              @q5wc.word_counter[word["text"]] ||= 0
+              @q5wc.word_counter[word["text"]] += word["count"]
+            end
+          end
+          @q5wc.save
         end
-        @q5wc.save
       end
     end
   end
